65bab2c37e1165e3dcff250b77b9bf60f329adf8
Rafael Jaques <rafael@phpit.com.br>
2024-06-20 09:21:26 -0300

    Aula 20/06

--- app/Http/Controllers/AnimaisController.php
+++ app/Http/Controllers/AnimaisController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Animal;
 use Illuminate\Http\Request;
 
 class AnimaisController extends Controller
 {
     public function index() {
-        return view('animais.index');
+        $dados = Animal::all();
+        
+        return view('animais.index', [
+            'animais' => $dados,
+        ]);
     }
 
     public function cadastrar() {
@@ -15,7 +20,19 @@ class AnimaisController extends Controller
     }
 
     public function gravar(Request $form) {
-        #dd($form);
-        echo $form->nome;
+        $dados = $form->validate([
+            'nome' => 'required|min:3',
+            'idade' => 'required|integer'
+        ]);
+
+        Animal::create($dados);
+        
+        return redirect()->route('animais');
+    }
+
+    public function apagar(Animal $animal) {
+        return view('animais.apagar', [
+            'animal' => $animal,
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,7 @@ Route::get('/animais', [AnimaisController::class, 'index'])->name('animais');
 Route::get('/animais/cadastrar', [AnimaisController::class, 'cadastrar'])->name('animais.cadastrar');
 
 Route::post('/animais/cadastrar',[AnimaisController::class, 'gravar'])->name('animais.gravar');
+
+Route::get('/animais/apagar/{animal}', [AnimaisController::class, 'apagar'])->name('animais.apagar');
+
+Route::delete('/animais/apagar/{animal}', [AnimaisController::class, 'apagar']);
